291328bdecf2d3cd03ceda3c4036ff6d44ee493a
Carlos Bonetti <carlosb_bc@hotmail.com>
2015-04-07 19:50:08 -0300

    Point clipping fixed

--- lib/cg/ClippingStrategy.cpp
+++ lib/cg/ClippingStrategy.cpp
@@ -7,15 +7,15 @@ namespace CG {
 
   bool SimplePointClipping::clipPoint(GPoint& p, const ClippingRect& rect) {
     assert(p.numPoints() == 1);
-
     auto c = p.coordinates()[0];
     bool onScreen = c.x >= rect.minX && c.x <= rect.maxX &&
                     c.y >= rect.minY && c.y <= rect.maxY;
 
-    if (!onScreen)
+    if (!onScreen) {
       p.clear();
+    }
 
-    return !onScreen;
+    return onScreen;
   }
 
   bool LBLineClipping::clipLine(GLine& l, const ClippingRect& rect) {
